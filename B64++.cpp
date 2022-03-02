822cee97b6817c62a46e7e0c60454c859fcef22e
StellarClown <salvo.sl70@gmail.com>
2022-03-02 17:51:43 +0100

    Update B64++.cpp

--- B64++.cpp
+++ B64++.cpp
@@ -1,13 +1,27 @@
-﻿#include <iostream>
+#include <iostream>
+#include <string>
 #include "Base64.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	string testA = "YOUR STRING HERE";
-	Base64 prova = {testA};
+	string commandList = argv[2];
+	if ((argc != 3) || (commandList != "-e")) {
+		cout << "[*] USAGE: " << argv[0] << " YOUR_MESSAGE -e" << endl;
+		return 0;
+	}
 
-	cout << prova.encodeB64() << endl;
+	cout << argv[1] << endl;
+
+	char command = argv[2][1];
+	Base64 b64data = argv[1];
+	switch (command) {
+		case 'e':
+			cout << "Encoded: " << b64data.encodeB64() << endl;
+			break;
+		default:
+			break;
+	}
 
 	return 0;
 }

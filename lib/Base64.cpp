6f752c6fd4049dd0b993a2d6ff1be2d3a206a177
StellarClown <salvo.sl70@gmail.com>
2022-03-08 19:05:17 +0100

    Refactoring

--- lib/Base64.cpp
+++ lib/Base64.cpp
@@ -1,11 +1,7 @@
-#include <algorithm>
 #include <cmath>
-#include <sstream>
 #include <string>
 #include "Base64.h"
 
-using std::stringstream;
-using std::reverse;
 using std::string;
 
 
@@ -25,67 +21,93 @@ Base64::Base64(const char* text) : msgPlainText(text) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
 /*
 	LOGIC
 */
-string Base64::fromStringToBinaryDigit() const {
-	string finalSeqBit;
-
-	//I convert the whole string to a sequence of bits
-	for (uint8_t x : this->msgPlainText) {
-		uint8_t maskToApply = 1;
-		stringstream toBin;
-
-		//I extract the bits of the single character
-		for (uint8_t i = 0; i < 8; i++) {
-			if (x & maskToApply)
-				toBin << "1";
-			else
-				toBin << "0";
-			maskToApply <<= 1;
-		}
-
-		string toAppend = toBin.str();
-		reverse(toAppend.begin(), toAppend.end());
-
-		finalSeqBit += toAppend;
-	}
-
-	return finalSeqBit;
-}
-
 string Base64::encodeText() const {
-	string toRet = "";
-	
+	string encodedString = "";
+
+	uint8_t indexNewChar = 0;
+	int8_t binSelection = 5;
+
 	//I divide the sequence of bits into groups of 6 and extract the character from the table
-	string bitText = fromStringToBinaryDigit();
-	int8_t binSel = 5;
-	uint8_t indexTable = 0;
-	for (char bin : bitText) {
-		if (binSel < 0) {
-			toRet += table[indexTable];
-			binSel = 5;
-			indexTable = 0;
+	for (const uint8_t character : this->msgPlainText) {
+		for (uint8_t i = 0, bitSelected = 0x80; i < 8; bitSelected >>= 1, i++) {
+			if (binSelection < 0) {
+				encodedString += this->table[indexNewChar];
+				binSelection = 5;
+				indexNewChar = 0;
+			}
+
+			if (character & bitSelected)
+				indexNewChar += static_cast<uint8_t>(pow(2, binSelection));
+
+			binSelection--;
 		}
-		if (bin == '1')
-			indexTable += pow(2, binSel);
-		binSel--;
 	}
-	toRet += table[indexTable];
+	encodedString += this->table[indexNewChar];
 
 	//I add a padding of '=' from 0 to 2 until the string is a multiple of 4
-	while (toRet.length() % 4)
-		toRet += "=";
+	while (encodedString.length() % 4) 
+		encodedString += "=";
 
-	return toRet;
+	return encodedString;
 }
 
+string Base64::decodeText() const {
+	string rmPadding = this->msgPlainText;
+	rmPadding.erase(remove(rmPadding.begin(), rmPadding.end(), '='), rmPadding.end());
+
+	string decodedString = "";
+
+	//TODO decoding algorithm
+
+	return decodedString;
+}
+
+
+
+
+
+
+
+
+
+/*
+	PUBLIC INTERFACE
+*/
 string Base64::encodeB64() const {
 	if (!this->msgPlainText.length())
 		return "";
 
 	return encodeText();
 }
+
+string Base64::decodeB64() const {
+	if (!this->msgPlainText.length())
+		return "";
+
+	return decodeText();
+}
+
+
+
+
+
+
+
+
+
 
 
 /*

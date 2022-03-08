62c84469f3d3550bf814748b03c36656229e6bd8
StellarClown <salvo.sl70@gmail.com>
2022-03-08 19:04:10 +0100

    Update B64++.cpp

--- B64++.cpp
+++ B64++.cpp
@@ -6,18 +6,19 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	string commandList = argv[2];
-	if ((argc != 3) || (commandList != "-e")) {
-		cout << "[*] USAGE: " << argv[0] << " YOUR_MESSAGE -e" << endl;
+	if ((argc != 3) || ((commandList != "-e") && (commandList != "-d"))) {
+		cout << "[*] USAGE: " << argv[0] << " YOUR_MESSAGE -e/-d" << endl;
 		return 0;
 	}
-
-	cout << argv[1] << endl;
 
 	char command = argv[2][1];
 	Base64 b64data = argv[1];
 	switch (command) {
 		case 'e':
 			cout << "Encoded: " << b64data.encodeB64() << endl;
+			break;
+		case 'd':
+			cout << "Decoded: " << b64data.decodeB64() << endl;
 			break;
 		default:
 			break;
